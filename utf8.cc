7cf7d16ee3aa30bf1705783f5c88f68439128d32
lisdude <git@lisdude.com>
2018-11-16 03:11:56 -0600

    Vastly increase the speed of utf8_strlen.

--- utf8.cc
+++ utf8.cc
@@ -30,52 +30,64 @@
 
 #include "utf8.h"
 #include "storage.h"
+#include "log.h"
 
-
-size_t utf8_strlen(const char* str)
-{
-    char *p = (char*)str;
-    int n = 0;
-    int state = 0;
+#define ONEMASK ((size_t)(-1) / 0xFF)
     
-    /* to ensure that this code doesn't miss the boundry of the string,
-     * we have to iterate more slowly than we could if we just jumped
-     * ahead when we found the number of bytes in a character.
-     */
-    while (*p != '\0')
-    {
-	if (state == 0)
-	{
-	    state = utf8_numbytes(*p) - 1;
-	    
-	    if (state == 0)
-	    {
-		n++;
-	    }
-	    else if (state == -2) /* -1 error val of utf8_numbytes minus one */
-	    {
-		/* just ignore this weird character */
-		state = 0;
-	    }
-	}
-	else
-	{
-	    /* There is no check here for the validity of the UTF-8 bytes
-	     * because I can't think of an appropriate failsafe behaviour
-	     * anyways.
-	     */
+    size_t
+utf8_strlen(const char * _s)
+{
+	const char * s;
+	size_t count = 0;
+	size_t u;
+	unsigned char b;
 
-	    if (0 == --state)
-	    {
-		n++;
-	    }
+	/* Handle any initial misaligned bytes. */
+	for (s = _s; (uintptr_t)(s) & (sizeof(size_t) - 1); s++) {
+		b = *s;
+
+		/* Exit if we hit a zero byte. */
+		if (b == '\0')
+			goto done;
+
+		/* Is this byte NOT the first byte of a character? */
+		count += (b >> 7) & ((~b) >> 6);
 	}
 
-	p++;
-    }
+	/* Handle complete blocks. */
+	for (; ; s += sizeof(size_t)) {
+		/* Prefetch 256 bytes ahead. */
+		__builtin_prefetch(&s[256], 0, 0);
 
-    return n;
+		/* Grab 4 or 8 bytes of UTF-8 data. */
+		u = *(size_t *)(s);
+
+		/* Exit the loop if there are any zero bytes. */
+		if ((u - ONEMASK) & (~u) & (ONEMASK * 0x80))
+			break;
+
+		/* Count bytes which are NOT the first byte of a character. */
+		u = ((u & (ONEMASK * 0x80)) >> 7) & ((~u) >> 6);
+		count += (u * ONEMASK) >> ((sizeof(size_t) - 1) * 8);
+	}
+
+	/* Take care of any left-over bytes. */
+	for (; ; s++) {
+		b = *s;
+
+		/* Exit if we hit a zero byte. */
+		if (b == '\0')
+			break;
+
+		/* Is this byte NOT the first byte of a character? */
+		count += (b >> 7) & ((~b) >> 6);
+	}
+
+done:
+	return ((s - _s) - count);
 }
+
+
 
 const char* utf8_index(const char* str, int32 index)
 {
